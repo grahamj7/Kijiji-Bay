3739328eb9f15e0e058e8ef4118f7d637ba12658
David <david.thai@usask.ca>
2014-04-02 18:39:55 -0600

    Allowed PHP scripts to be Crossed-origin resource sharing

--- php scripts/get_all_items.php
+++ php scripts/get_all_items.php
@@ -1,5 +1,6 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
 $tableName ="Items";
 $ItemIDColName= "ItemId";
 $TitleColName="Title";
--- php scripts/get_all_users.php
+++ php scripts/get_all_users.php
@@ -1,5 +1,6 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');  
+header('Content-type: application/json');
 $tableName ="Users";
 
 $Col1Name="UserId";
