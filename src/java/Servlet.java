dd17f3c80cf77d8cb6d20f4f519f7083309e3355
Thaid <david.thai22@gmail.com>
2014-04-08 10:46:45 -0600

    Added more database functionality

--- src/java/Servlet.java
+++ src/java/Servlet.java
@@ -29,7 +29,7 @@ public class Servlet extends HttpServlet {
     public final String user="cmpt350_jd";
     public final String password="7jksf72834jk";
     
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    public void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
         HttpSession session = request.getSession(true);
@@ -68,7 +68,7 @@ public class Servlet extends HttpServlet {
         
     }
     
-    protected void outputResult(HttpServletRequest request, HttpServletResponse response, HttpSession session)
+    public void outputResult(HttpServletRequest request, HttpServletResponse response, HttpSession session)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
@@ -76,7 +76,7 @@ public class Servlet extends HttpServlet {
 
     }
     
-    public void getConnection()
+    protected void getConnection()
     {
         try {
             DriverManager.registerDriver(new com.mysql.jdbc.Driver());
@@ -88,8 +88,39 @@ public class Servlet extends HttpServlet {
             System.out.println(e.toString());
         }
     }
-    
-   protected void  insertItem(String title, float price, String description, int mainCategory, int subCategory ){
+    public void closeConnection()
+    {
+        try{
+           this.conn.close();
+        }
+        catch(SQLException e)
+        {
+            System.out.println(e.toString());
+        }
+    }
+   protected int getMaxItemID()throws SQLException{
+       
+       this.getConnection();
+       final String query = "SELECT MAX(ItemId) FROM Items";
+       final PreparedStatement ps = this.conn.prepareStatement(query);
+       final ResultSet result = ps.executeQuery();
+       
+       if(result.next()){
+           final int itemID = result.getInt(1);
+           this.closeConnection();
+           return itemID;
+       }
+       else if(!result.next()){
+           this.closeConnection();
+           return 0;
+       }
+       else{
+           this.closeConnection();
+           throw new SQLException();
+       }
+	
+   }
+   public void  insertItem(String title, float price, String description, int mainCategory, int subCategory ){
         try{
             this.getConnection();
             String main=Integer.toString(mainCategory);
@@ -97,8 +128,7 @@ public class Servlet extends HttpServlet {
             final String query = "INSERT INTO Items(ItemId,Title,Description,Price,MainCategory,SubCategory) "
                     + "VALUES ( ? , ? , ? , ? , ? , ? )";
             final PreparedStatement ps = this.conn.prepareStatement(query);
-            Random rand = new Random();
-            int itemID=rand.nextInt(100);
+            int itemID=getMaxItemID()+1;
             ps.setInt(1, itemID);
             ps.setString(2, title);
             ps.setString(3, description);
@@ -106,12 +136,13 @@ public class Servlet extends HttpServlet {
             ps.setString(5,main);
             ps.setString(6, sub);
             ps.executeUpdate();
+            this.closeConnection();
         }catch (SQLException e){
             System.out.println(e.toString());
         }
     }
    
-   protected void getItems(HttpServletRequest request, HttpServletResponse response, HttpSession session, int main, int sub)
+   public void getItems(HttpServletRequest request, HttpServletResponse response, HttpSession session, int main, int sub)
             throws ServletException, IOException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
@@ -167,11 +198,84 @@ public class Servlet extends HttpServlet {
             out.println("</tr>");
         }
         out.println("</table>");
-       
-        
-        
-        
+
     }
+   
+   public void insertUser(String username, String password, String fname, String lname, String email, int age, String phone, String address, String city, String prov, String postal){
+       try{
+            this.getConnection();
+            final String query = "INSERT INTO Users (Username, Password, Firstname, Lastname, Email, Age, Phonenumber, Address, City, Province, PostalCode) "
+                    + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            final PreparedStatement ps = this.conn.prepareStatement(query);
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ps.setString(3, fname);
+            ps.setString(4, lname);
+            ps.setString(5, email);
+            ps.setInt(6,age);
+            ps.setString(7, phone);
+            ps.setString(8, address);
+            ps.setString(9, city);
+            ps.setString(10, prov);
+            ps.setString(11, postal);     
+            ps.executeUpdate();
+            this.closeConnection();
+        }catch (SQLException e){
+            System.out.println(e.toString());
+        }
+   }
+   
+   public void updateUserInfo(String username, String password, String fname, String lname, String email, int age, String phone, String address, String city, String prov, String postal){
+       
+       try{
+           this.getConnection();
+           final String query = "UPDATE Users SET Password = ?, Firstname = ?, Lastname = ?, Email = ?, Age = ?, Phonenumber = ?, Address = ?, City = ?, Province = ?, PostalCode = ? WHERE Username = ?";
+           final PreparedStatement ps = this.conn.prepareStatement(query);
+ 
+           ps.setString(1, password);
+           ps.setString(2, fname);
+           ps.setString(3, lname);
+           ps.setString(4, email);
+           ps.setInt(5,age);
+           ps.setString(6, phone);
+           ps.setString(7, address);
+           ps.setString(8, city);
+           ps.setString(9, prov);
+           ps.setString(10, postal);
+           ps.setString(11, username);
+           ps.executeUpdate();
+           this.closeConnection();
+       }catch (SQLException e){
+           System.out.println(e.toString());
+       }
+   } 
+   public boolean checkIfUserExists(String username){
+       try{
+           this.getConnection();
+           final String query = "SELECT count(*) FROM Users WHERE username = ?";
+           final PreparedStatement ps = this.conn.prepareStatement(query);
+           ps.setString(1, username);
+           final ResultSet result = ps.executeQuery();
+           
+           /*
+           * If there is anything in the result, user must exist
+           */
+           if(result.next()){
+               final int count = result.getInt(1);
+               if(count == 1){
+                   return true;
+               }
+               else{
+                   return false;
+               }
+           }
+           this.closeConnection();
+           return false;
+       }catch (SQLException e){
+           System.out.println(e.toString());
+       }
+       return false;
+   }
     
 // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
